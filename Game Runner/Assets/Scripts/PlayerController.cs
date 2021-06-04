d4645df679e3d85fdad1bca5cdee15e68091a424
MilliBom <millibom5@gmail.com>
2021-06-04 19:55:06 +0300

    The underwater runner game

---
+++ Game Runner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    //public float moveSpeed;
+    float jumpForce = 8f;
+    private int jumpCounter;
+    private int jumpCountMax = 1;
+
+    Rigidbody2D rb;
+    Collider2D myCollider, myTopCollider;
+
+    public bool isGrounded, isTopGrounded;
+    public LayerMask whatIsGround, whatIsTopGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
+        myTopCollider = GetComponent<Collider2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+        isTopGrounded = Physics2D.IsTouchingLayers(myTopCollider, whatIsTopGround);
+        
+        if (isGrounded == true)
+        {
+            jumpCounter = 0;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded == true || isTopGrounded == true) 
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                rb.gravityScale = 1;
+            }
+            else
+            {
+                if (jumpCounter < jumpCountMax)
+                {
+                    rb.velocity = Vector2.up * jumpForce;
+                    jumpCounter++;
+                    
+                    if (jumpCounter == jumpCountMax)
+                    {
+                        rb.gravityScale = -1;                        
+                    }
+                }
+            }            
+        }        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {                
+        if (collision.gameObject.tag == "Enemy")
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
---
+++ Game Runner/Assets/Scripts/ScoreManagerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int score;
+    public Text scoreTxt;
+    private void Update()
+    {
+        scoreTxt.text = "Score: " + score.ToString();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            score++;
+            //Debug.Log(score);
+        }
+    }
+}
